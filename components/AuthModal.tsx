09d2af489a63f3fed8c1cbbc1d0ad61fe82b1d18
Sina <sina.meraji@gmail.com>
2025-02-06 16:09:01 +0800

    add confetti when user creates a new resource

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -9,7 +9,7 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog"
 import { createClient } from '@/utils/supabase/client';
-
+import confetti from "canvas-confetti";
 import {
     Form,
     FormControl,
@@ -59,6 +59,34 @@ export function AuthModal() {
 
     function onSubmit(values: z.infer<typeof formSchema>) {
         signInWithEmail(values.email)
+        const defaults = {
+          spread: 360,
+          ticks: 50,
+          gravity: 0,
+          decay: 0.94,
+          startVelocity: 30,
+          colors: ["#FFE400", "#FFBD00", "#E89400", "#FFCA6C", "#FDFFB8"],
+        };
+     
+        const shoot = () => {
+          confetti({
+            ...defaults,
+            particleCount: 40,
+            scalar: 1.2,
+            shapes: ["star"],
+          });
+     
+          confetti({
+            ...defaults,
+            particleCount: 10,
+            scalar: 0.75,
+            shapes: ["circle"],
+          });
+        };
+     
+        setTimeout(shoot, 0);
+        setTimeout(shoot, 100);
+        setTimeout(shoot, 200);
     }
 
   return (
--- components/CreateResourceModal.tsx
+++ components/CreateResourceModal.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import { createClient } from '@/utils/supabase/client';
 import { v4 as uuidv4 } from 'uuid'; 
 import { AuthModal } from './AuthModal';
+import confetti from "canvas-confetti";
 
 type ResourceForm = {
   title: string;
@@ -72,8 +73,38 @@ export function CreateResourceModal({ category, setRefetchResources, onResourceC
       onResourceCreated();
     }
     setLoading(false);
+    
+    const end = Date.now() + 3 * 1000; // 3 seconds
+    const colors = ["#a786ff", "#fd8bbc", "#eca184", "#f8deb1"];
+ 
+    const frame = () => {
+      if (Date.now() > end) return;
+ 
+      confetti({
+        particleCount: 2,
+        angle: 60,
+        spread: 55,
+        startVelocity: 60,
+        origin: { x: 0, y: 0.5 },
+        colors: colors,
+      });
+      confetti({
+        particleCount: 2,
+        angle: 120,
+        spread: 55,
+        startVelocity: 60,
+        origin: { x: 1, y: 0.5 },
+        colors: colors,
+      });
+ 
+      requestAnimationFrame(frame);
+    };
+ 
+    frame();
   };
 
+
+  
   return (
     <>
       {showAuthModal && <AuthModal />}
